b7542581c5899ad88d064ec6825206aa2d026564
GurkhaShieldForce <168552262+GurkhaShieldForce@users.noreply.github.com>
2025-02-08 10:42:01 -0500

    Added frontend url to middleware
    Added frontend url to middleware

--- server/server.js
+++ server/server.js
@@ -111,7 +111,7 @@ const startServer = async () => {
 
     // Middleware setup
     app.use(cors({ 
-      origin: ['http://localhost:5173'],
+      origin: ['https://cookconnect-six.vercel.app/'],
       credentials: true 
     }));
     app.use(express.json());
